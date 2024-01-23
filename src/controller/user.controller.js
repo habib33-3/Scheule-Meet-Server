487a2fc264b7bd34010d0c910b978e332937b739
habib33-3 <habib.swi16@gmail.com>
2024-01-24 00:32:24 +0600

    create api for save cookie after login

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,5 +1,5 @@
 import { User } from "../models/user.model.js";
-
+import jwt from "jsonwebtoken";
 
 // controller for save user to db
 const saveUserToDb = async (req, res) => {
@@ -30,4 +30,25 @@ const saveUserToDb = async (req, res) => {
     }
 };
 
-export { saveUserToDb };
+// create jwt token after login or register
+const createToken = async (req, res) => {
+    try {
+        const user = req.body;
+        const token = jwt.sign(user, process.env.ACCESS_TOKEN, {
+            expiresIn: "24h",
+        });
+        res.cookie("token", token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        }).send({ success: true });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: "Server Error during creating token",
+            success: false,
+        });
+    }
+};
+
+export { saveUserToDb, createToken };
