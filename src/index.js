92509a62bd2b5b9fa10d7fd952650f1f9c70e3b7
habib33-3 <habib.swi16@gmail.com>
2024-01-24 20:31:29 +0600

    required env variables for project

--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
 import { connectDB } from "./db/db.js";
+import "dotenv/config";
+
+const envVariables = [
+    "DB_USER",
+    "DB_PASS",
+    "ACCESS_TOKEN",
+    "CLIENT_DEV",
+    "CLIENT_PROD",
+];
+
+envVariables.forEach((envVar) => {
+    if (!process.env[envVar]) {
+        console.error(
+            `Error: Environment variable ${envVar} is not set. Please refer to the Readme file for details. Exiting...`
+        );
+        process.exit(1);
+    }
+});
 
 connectDB().catch((err) => {
     console.error("Error starting express server:", err);
