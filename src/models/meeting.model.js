220b706ac73bd5e7878122467a1d6cece82048ef
habib33-3 <habib.swi16@gmail.com>
2024-01-25 12:51:10 +0600

    created meeting schema

---
+++ src/models/meeting.model.js
@@ -0,0 +1,44 @@
+import { Schema, model } from "mongoose";
+
+const MeetingSchema = new Schema(
+    {
+        meetingTitle: {
+            type: String,
+            required: true,
+        },
+        hostName: {
+            type: String,
+            required: true,
+        },
+        hostEmail: {
+            type: String,
+            required: true,
+        },
+        guestName: {
+            type: String,
+            required: true,
+        },
+        guestEmail: {
+            type: String,
+            required: true,
+        },
+        date: {
+            type: Date,
+            required: true,
+        },
+
+        time: {
+            type: [String],
+            required: true,
+        },
+        meetingLink: {
+            type: String,
+            required: true,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+export const Meeting = model("meetings", MeetingSchema);
