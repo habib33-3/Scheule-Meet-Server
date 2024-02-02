818a24d20820eb11fd06156cd4f6f11b94a791bb
Habibur Rahman <habib.swi16@gmail.com>
2024-02-02 20:32:40 +0600

    credentials false

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(
     cors({
         origin: [process.env.CLIENT_DEV, process.env.CLIENT_PROD],
-        credentials: true
+        
         // credentials: true,
     })
 );
