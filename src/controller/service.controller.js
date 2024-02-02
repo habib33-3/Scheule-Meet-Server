c2eac7f496d825b7b0ce1d80e6f6d85cc2945134
habib33-3 <habib.swi16@gmail.com>
2024-02-02 17:25:02 +0600

    event and meeting emails

--- src/controller/service.controller.js
+++ src/controller/service.controller.js
@@ -1,7 +1,10 @@
 import receiveMail from "../services/receiveMail.js";
 import sendMail from "../services/sendMail.js";
 import contactUsTemplate from "../templates/contactUs.template.js";
+import eventCreateTemplate from "../templates/eventCreateConfirm.template.js";
+import eventInviteTemplate from "../templates/eventInvite.template.js";
 import meetingCreateTemplate from "../templates/meetingCreateConfirm.template.js";
+import meetingInviteTemplate from "../templates/meetingInvite.template.js";
 
 const receiveContactUs = async (req, res) => {
     try {
@@ -68,4 +71,114 @@ const sendMeetingCreateConfirmation = async (req, res) => {
     }
 };
 
-export { receiveContactUs, sendMeetingCreateConfirmation };
+const sendEventCreateConfirmation = async (req, res) => {
+    try {
+        const { email, hostName, dateTime, eventLink } = req.body;
+
+        const mailBody = eventCreateTemplate(hostName, dateTime, eventLink);
+
+        const sent = sendMail(
+            email,
+            "Your Event Created Successfully",
+            mailBody
+        );
+
+        if (sent) {
+            return res.status(200).json({
+                message: "Mail Sent Successfully",
+                success: true,
+            });
+        } else {
+            return res.status(400).json({
+                message: "Can't send Mail",
+                success: false,
+            });
+        }
+    } catch (error) {
+        console.error(
+            "Server Error during Event create confirmation email sent"
+        );
+        return res.status(500).json({
+            message: "Server Error during Event create confirmation email sent",
+            success: false,
+        });
+    }
+};
+
+const sendEventInvitation = async (req, res) => {
+    try {
+        const { hostName, guestEmail, dateTime } = req.body;
+
+        const mailBody = eventInviteTemplate(hostName, guestEmail, dateTime);
+
+        const sent = sendMail(
+            guestEmail,
+            "You got an invitation for an Event",
+            mailBody
+        );
+
+        if (sent) {
+            return res.status(200).json({
+                message: "Mail Sent Successfully",
+                success: true,
+            });
+        } else {
+            return res.status(400).json({
+                message: "Can't send Mail",
+                success: false,
+            });
+        }
+    } catch (error) {
+        console.error(
+            "Server Error during Event invitation confirmation email sent"
+        );
+        return res.status(500).json({
+            message:
+                "Server Error during Event invitation confirmation email sent",
+            success: false,
+        });
+    }
+};
+
+const sendMeetingInvitation = async (req, res) => {
+    try {
+        const { hostName, guestEmail, dateTime } = req.body;
+
+        const mailBody = meetingInviteTemplate(hostName, guestEmail, dateTime);
+
+        const sent = sendMail(
+            guestEmail,
+            "You got an invitation for an Meeting",
+            mailBody
+        );
+
+        if (sent) {
+            return res.status(200).json({
+                message: "Mail Sent Successfully",
+                success: true,
+            });
+        } else {
+            return res.status(400).json({
+                message: "Can't send Mail",
+                success: false,
+            });
+        }
+    } catch (error) {
+        console.error(
+            "Server Error during Meeting invitation confirmation email sent"
+        );
+        return res.status(500).json({
+            message:
+                "Server Error during Meeting invitation confirmation email sent",
+            success: false,
+        });
+    }
+};
+
+export {
+    receiveContactUs,
+    sendMeetingCreateConfirmation,
+    sendEventCreateConfirmation,
+    sendEventInvitation,
+    sendMeetingInvitation,
+};
--- src/routes/service.routes.js
+++ src/routes/service.routes.js
@@ -1,7 +1,10 @@
 import { Router } from "express";
 import {
     receiveContactUs,
+    sendEventCreateConfirmation,
+    sendEventInvitation,
     sendMeetingCreateConfirmation,
+    sendMeetingInvitation,
 } from "../controller/service.controller.js";
 
 const router = Router();
@@ -12,5 +15,13 @@ router.post("/contactUs", receiveContactUs);
 // sent meeting creation confirmation email
 router.post("/meetingCreate", sendMeetingCreateConfirmation);
 
+// sent event creation confirmation email
+router.post("/eventCreate", sendEventCreateConfirmation);
+
+// sent event invitation  email
+router.post("/eventInvite", sendEventInvitation);
+
+// sent meeting invitation  email
+router.post("/meetingInvite", sendMeetingInvitation);
 
 export default router;
