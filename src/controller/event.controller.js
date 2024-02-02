09d5b9632dc4df0073aa394e7e23c29d01d8f117
habib33-3 <habib.swi16@gmail.com>
2024-02-02 16:56:32 +0600

    route for update and delete event

--- src/controller/event.controller.js
+++ src/controller/event.controller.js
@@ -19,4 +19,55 @@ const createEvent = async (req, res) => {
     }
 };
 
-export { createEvent };
+const deleteEvent = async (req, res) => {
+    try {
+        const { id } = req.query;
+
+        await Event.findByIdAndDelete(id);
+
+        return res.status(200).json({
+            message: "Event deleted successfully",
+            success: true,
+        });
+    } catch (error) {
+        console.error("error during event delete".error);
+        return res.status(500).json({
+            message: "Error during event delete",
+            success: false,
+        });
+    }
+};
+
+const updateEvent = async (req, res) => {
+    try {
+        const { eventTitle, date, eventLink } = req.body;
+
+        const { id } = req.query;
+
+        const updateEvent = await Event.findByIdAndUpdate(
+            id,
+            {
+                eventTitle,
+                date,
+                eventLink,
+            },
+            {
+                new: true,
+            }
+        );
+
+        return res.status(200).json({
+            message: "Event deleted successfully",
+            success: true,
+            event: updateEvent,
+        });
+    } catch (error) {
+        console.error("error during event update");
+        return res.status(500).json({
+            message: "Error during event delete",
+            success: false,
+        });
+    }
+};
+
+export { createEvent, deleteEvent, updateEvent };
--- src/routes/event.routes.js
+++ src/routes/event.routes.js
@@ -1,9 +1,19 @@
 import { Router } from "express";
-import { createEvent } from "../controller/event.controller.js";
+import {
+    createEvent,
+    deleteEvent,
+    updateEvent,
+} from "../controller/event.controller.js";
 
 const router = Router();
 
 // route for create event
 router.post("/createEvent", createEvent); //TODO: private route
+
+// route for delete event
+router.delete("/deleteEvent/:id", deleteEvent); //TODO: verify token will be added
+
+// route for update event
+router.put("/updateEvent/:id", updateEvent); //TODO: verify token will be added
 
 export default router;
