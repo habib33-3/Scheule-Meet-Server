4457393a93bbc7617498aa1deb4fc20bf7873bdc
habib33-3 <habib.swi16@gmail.com>
2024-02-07 00:33:08 +0600

    delete report

--- src/controller/bannedUser.controller.js
+++ src/controller/bannedUser.controller.js
@@ -1,7 +1,6 @@
 import { BannedUsers } from "../models/bannedUser.model";
 import { User } from "../models/user.model";
 
-
 const banUser = async (req, res) => {
     try {
         const banInfo = req.body;
@@ -22,7 +21,7 @@ const banUser = async (req, res) => {
             }
         );
 
-        //    Todo: mail will be implemented
+        //    TODO: mail will be implemented
 
         res.status(200).json({
             message: "User banned Successfully",
@@ -37,4 +36,27 @@ const banUser = async (req, res) => {
     }
 };
 
-export { banUser };
+const unBanUser = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const query = { _id: id.id };
+
+        await BannedUsers.deleteOne(query);
+
+        //    TODO: mail will be implemented
+
+        res.status(200).json({
+            message: "User unBanned Successfully",
+            success: true,
+        });
+    } catch (error) {
+        console.error("Server error during unBan user", error);
+        res.status(500).json({
+            message: "Server error during unBan user",
+            success: false,
+        });
+    }
+};
+
+export { banUser, unBanUser };
--- src/routes/bannedUser.routes.js
+++ src/routes/bannedUser.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { banUser } from "../controller/bannedUser.controller";
+import { banUser, unBanUser } from "../controller/bannedUser.controller";
 
 const router = Router();
 
 // route to ban user
 router.post("/banUser", banUser); //TODO: verify token and verify admin will be added
 
+// route to unBan user
+router.delete("/unBan/:id", unBanUser);
 
-export default router
+export default router;
