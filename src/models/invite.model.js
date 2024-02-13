7b351cde354e313b6f55f38e525d7333b04cf7b3
habib33-3 <habib.swi16@gmail.com>
2024-02-13 21:31:27 +0600

    invite user

---
+++ src/models/invite.model.js
@@ -0,0 +1,31 @@
+import { Schema, model } from "mongoose";
+
+const InviteSchema = new Schema({
+    invitedEmail: {
+        type: String,
+        required: true,
+    },
+
+    eventType: {
+        type: String,
+        enums: ["event", "meeting"],
+    },
+    eventId: {
+        type: Schema.Types.ObjectId,
+        required: true,
+    },
+    eventTitle: {
+        type: String,
+        required: true,
+    },
+    date: {
+        type: Date,
+        required: true,
+    },
+
+    type: String,
+    enum: ["accepted", "rejected", "pending"],
+    default: "pending",
+});
+
+export const Invite = model("invites", InviteSchema);
