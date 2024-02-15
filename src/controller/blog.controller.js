b2c15e757d748da8bc9c10ec126b341e34f0fa52
mdfaisalgithupe <mdfaisald0706@gmail.com>
2024-02-15 09:41:31 +0600

    J

---
+++ src/controller/blog.controller.js
@@ -0,0 +1,28 @@
+import { blogModel } from "../models/blog.model.js";
+
+//! get specific blog , baed on id ( get single blog )
+const SingleBlog = async (req, res) => {
+const { id } = req.params;
+const result = await blogModel.find({_id: id});
+  res.send(result)
+};
+
+const addBlog = async (req, res) => {
+const blog = req.body;
+const result = await blogModel.create(blog);
+res.send(result)
+
+};
+
+const getBlogs = async (req, res) => {
+const result = await blogModel.find();
+res.send(result)
+
+
+};
+
+
+
+
+
+export {addBlog, getBlogs, SingleBlog };
